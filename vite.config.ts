9dbe4f0ed014a29bad70f7262e31ce1f5651a347
Vonter <25414711+Vonter@users.noreply.github.com>
2024-12-28 09:48:10 +0530

    Add analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import postcss from './postcss.config.js'
+import postcss from './postcss.config.js';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  console.log('Building for /', process.env.VITE_BASE_URL)
+  console.log('Building for /', process.env.VITE_BASE_URL);
 
   return defineConfig({
     css: { postcss },
     preprocess: [vitePreprocess({ script: true })],
-    plugins: [svelte()],
+    plugins: [
+      svelte(),
+      VitePluginRadar({
+        analytics: {
+          id: process.env.VITE_GOOGLE_ANALYTICS_ID
+        }
+      })
+    ],
     base: process.env.VITE_BASE_URL ? `/${process.env.VITE_BASE_URL}/` : './'
   });
-}
+};
