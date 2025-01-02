869aa873d7f8472e2e1e89e71f51756503c17e33
Vonter <25414711+Vonter@users.noreply.github.com>
2025-01-02 14:17:45 +0530

    Configure adapter-cloudflare for SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   },
   preprocess: preprocess()
 };
